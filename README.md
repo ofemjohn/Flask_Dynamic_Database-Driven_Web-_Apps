b292938e65d298920f9fbde64c10ef9518886c2f
ofemjohn <ofemjohn@gmail.com>
2023-03-03 21:49:50 +0100

    in progress

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 Author (John Ofem, +2347068559023)
 
+https://getbootstrap.com/docs/5.3/examples/     ######bootstrap
+https://css-tricks.com/snippets/html/mailto-links/  ##### default mail programms
+https://htmldog.com/references/css/properties/ ### html & css
+https://jovian.com/aakashns/web-development-with-python
+https://jovian.com/aakashns/database-driven-web-applications
+https://mailtolink.me/   #### mail links
+https://excalidraw.com/ ### for mockup sketches 
+
+
+
+
 
 # A Carrier Website For John Teacher Farms
--- app.py
+++ app.py
@@ -2,9 +2,42 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+{
+    "id": 1,
+    "title": "MERN STACK",
+    "location": "Bengalrus, India",
+    "salary": "Rs. 1,00,00",
+    "email": "www.john@gmail.com"
+},
+{
+    "id": 2,
+    "title": "FULL STACK",
+    "location": "China",
+    "salary": "Rs. 2,00,00",
+    "email": "jhfg@gmail.com"
+},
+{
+    "id": 3,
+    "title": "DYSLEXIA",
+    "location": "LAGOS, NIGERIA",
+    "salary": "NG 345,000",
+    "email": "dyslexia@gmail.com"
+},
+{
+    "id": 4,
+    "title": "COMPUTER SCIENCE",
+    "location": "GHANA",
+    "salary": "12345",
+    "email": "favour@gmail.com"
+}
+
+]
+
 @app.route("/")
+@app.route("/home")
 def hello_world():
-    return render_template('/index.html')
+    return render_template('/index.html', jobs=JOBS)
 
 
 if __name__ == "__main__":
